614a3f19e72d5e3490cfee19a139edca56a0b03b
p <m>
2017-09-25 20:14:40 +0200

    next

--- operatory.js
+++ operatory.js
@@ -3,10 +3,8 @@ var b = prompt('Type the value of "b"');
 var value = (a * a) + (2 * a * b) - (b * b);
 if (value > 0) {
 	console.log('wynik dodatni!');
-}
-else if (value < 0) {
+} else if (value < 0) {
 	console.log('wynik ujemny');
-}
-else {
+} else {
 	console.log('wynik jest równy 0!');
 }
